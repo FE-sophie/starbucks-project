6a144bcc6bb62fd5cd8c92d997f7f6d65f1598bb
Jeong-seyuong <jsujsu4164@naver.com>
2020-08-07 09:11:09 +0900

    dddd

--- js/main.js
+++ js/main.js
@@ -29,21 +29,20 @@
 		
 // 		})	
 	
-
+$('.total-search__input').hide(300)
 	$('.total-search__btn').click(function(){
-			// alert('검색창 열림');
-			
+			var ref=$(this).attr('type');
 			$(this).addClass('total--search__btn--act');
-			$(this).attr('type','submit');
-			alert('dd')
+			$('.total-search__input').show(300);
+			alert(ref)
+
+			// $(this).attr('type','submit');
 			// $('.total--search__btn--act').attr('type','button');
 
 			// $('.total--search__btn--act').submit(function(){
-			
-				
-
 })
 $('.total--search__btn--act').click(function(){
+	alert(ref)
 	var val=$('.total-search__input').val()
 	if(val==""){
 		$('.total-search__input').focus()
@@ -51,7 +50,13 @@ $('.total--search__btn--act').click(function(){
 	}
 	else{
 		$(this).attr('type','submit');
-		// $(this).removeClass('total--search__btn--act')
+
+		// $('.total--search__btn--act').submit(function(){
+		// 	$(this).removeClass('total--search__btn--act');
+		// 	$(this).attr('type','button');
+		// 	$('.total-search__input').hide(1)
+		// })
+		
 	}
 })
 

d8afe033e93c9a3d83a2f5bfc662720d8796b33e
Jeong-seyoung <jsujsu4164@naver.com>
2020-08-10 12:39:11 +0900

    제이쿼리수정

--- js/main.js
+++ js/main.js
@@ -1,62 +1,51 @@
-// $(function(){
-// 	$('.main-menu-panel__item').hide(); //2dep 안보이게 처리함.
-// 	$('.nav__main-menu li a').on({
-// 		click:function(){
-// 			var ref=$(this).parents('li').attr('id');
-// 			// var aria=$('.main-menu-panel__item').attr('aria-labelledby')
-
-// 				$("li[aria-labelledby*="+ref+"]").addClass('act').siblings().removeClass('act');
-// 				$("li[aria-labelledby*="+ref+"]").slideToggle(300)
-
-// 		},
-// 		mouseover:function(){
-// 			var ref=$(this).parents('li').attr('id');
-// 			// var aria=$('.main-menu-panel__item').attr('aria-labelledby')
-// 			$("li[aria-labelledby*="+ref+"]").addClass('act').siblings().removeClass('act');
-// 			$("li[aria-labelledby*="+ref+"]").slideDown(300);
-// 		},
-// 			// $('.main-menu-panel__list>li').mouseleave(function(){
-// 			// 	$(this).slideUp(300)
-// 			// })
-// 		//},
-// 		mouseleave:function(){
-// 			$('.act').slideUp(300);
-// 		},
-// 		blur:function(){
-// 			$('.act').slideUp(300); 	
-// 		}
-// 	});
-		
-// 		})	
+$(function(){
+	$('.main-menu-panel__item').hide(); //2dep 안보이게 처리함.
+	$('.nav__main-menu li a').on({
+		click:function(){
+			var ref=$(this).parents('li').attr('id');
+			// var aria=$('.main-menu-panel__item').attr('aria-labelledby')
+			
+			$("li[aria-labelledby*="+ref+"]").addClass('act').siblings().removeClass('act');
+				$("li[aria-labelledby*="+ref+"]").slideToggle(100)
+				
+		},
+		mouseover:function(){
+			var ref=$(this).parents('li').attr('id');
+			// var aria=$('.main-menu-panel__item').attr('aria-labelledby')
+			$("li[aria-labelledby*="+ref+"]").addClass('act').siblings().removeClass('act');
+			$("li[aria-labelledby*="+ref+"]").slideDown(300);
+			$("li[aria-labelledby*="+ref+"]").mouseleave(function(){
+				$(this).hide()
+			})
+		},
+		mouseleave:function(){
+			// $('.main-menu-panel__item').hide()
+			
+		},
+		blur:function(){
+			$('.act').slideUp(100); 	
+		}
+	})
 	
+	
+		
+})	
 $('.total-search__input').hide(300)
-	$('.total-search__btn').click(function(){
-			var ref=$(this).attr('type');
-			$(this).addClass('total--search__btn--act');
-			$('.total-search__input').show(300);
-			alert(ref)
-
-			// $(this).attr('type','submit');
-			// $('.total--search__btn--act').attr('type','button');
-
-			// $('.total--search__btn--act').submit(function(){
-})
-$('.total--search__btn--act').click(function(){
-	alert(ref)
+$('.total-search__btn').click(function(){
+	var ref=$(this).attr('type');
+	$('.total-search__input').show(300);
+	$(this).attr('type','submit')
+	$(this).addClass('total--search__btn--act');
+	// alert(ref)
 	var val=$('.total-search__input').val()
 	if(val==""){
 		$('.total-search__input').focus()
-		$(this).attr('type','button');
 	}
 	else{
-		$(this).attr('type','submit');
-
-		// $('.total--search__btn--act').submit(function(){
-		// 	$(this).removeClass('total--search__btn--act');
-		// 	$(this).attr('type','button');
-		// 	$('.total-search__input').hide(1)
-		// })
-		
+		$('.total-search__input').submit();
+		$('.total-search__input').hide(300)
+		$(this).removeClass('total--search__btn--act')
+		$(this).attr('type','button')
 	}
 })
 
